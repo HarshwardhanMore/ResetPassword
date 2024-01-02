8b79c876f7789a26b6f5d4e85cc9d92cd6b8f5b7
Harshwardhan More <harshawardhanmore14@gmail.com>
2024-01-02 16:29:46 +0530

    working forgot password API with ejs without

--- server/services/resetPasswordService.js
+++ server/services/resetPasswordService.js
@@ -1,3 +1,4 @@
+const config = require('../config');
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 
@@ -9,7 +10,7 @@ let user = {
     password: "abc"
 }
 
-exports.resetPassword = async ({id, token, password})=>{
+exports.resetPassword = async ({id, token, password1})=>{
 
     // try {
     //     const newUser = await User.create(userDetails)
@@ -18,6 +19,8 @@ exports.resetPassword = async ({id, token, password})=>{
     //     throw new Error("Error adding user enquiry"+error.message);
     // }
 
+    console.log("Recieved password : "+ password1);
+    console.log("Old password  : "+ user.password);
 
     // check if this id exist in database
     if(id != user.id){
@@ -26,7 +29,7 @@ exports.resetPassword = async ({id, token, password})=>{
     }
     
     // we have valid id and user
-    const secret = JWT_SECRET + user.password;
+    const secret = config.JWT_SECRET + user.password;
 
     try {
         const payload = jwt.verify(token, secret); // if this succeeds then below code runs else catch block
@@ -35,10 +38,12 @@ exports.resetPassword = async ({id, token, password})=>{
 
         // find the user with payload id and email to make changes in password in database.
         // hash the password before sending.
-        user.password = password;
+        user.password = password1;
         // res.send(user);
         console.log("Updated user");
         console.log(user);
+        
+        console.log("New password : "+ user.password);
 
     } catch (error) {
         console.log(error.message);
